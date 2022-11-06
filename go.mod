4c989cc904b172cedd439608df2f4f87f413156c
Zeynep Gül Köle <zeynep0339@gmail.com>
2022-11-07 02:07:53 +0300

    sa

---
+++ go.mod
@@ -0,0 +1,24 @@
+module golang-middleware
+
+go 1.19
+
+require (
+	github.com/andybalholm/brotli v1.0.4 // indirect
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/gofiber/fiber v1.14.6 // indirect
+	github.com/gofiber/fiber/v2 v2.39.0 // indirect
+	github.com/gofiber/jwt/v3 v3.3.3 // indirect
+	github.com/gofiber/utils v0.0.10 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
+	github.com/golang-jwt/jwt/v4 v4.4.2 // indirect
+	github.com/gorilla/schema v1.1.0 // indirect
+	github.com/klauspost/compress v1.15.12 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.41.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+
+	jwtware "github.com/gofiber/jwt/v3"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+const jwtSecret = "asecret"
+
+func main() {
+	app := fiber.New()
+
+	// Login route
+	app.Post("/login", login)
+
+	// Unauthenticated route
+	app.Get("/", accessible)
+
+	// JWT Middleware
+	app.Use(jwtware.New(jwtware.Config{
+		SigningKey: []byte(jwtSecret),
+	}))
+
+	// Restricted Routes
+	app.Get("/restricted", restricted)
+
+	app.Listen(":3000")
+}
+
+func login(c *fiber.Ctx) error {
+	type request struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
+	}
+
+	var body request
+	err := c.BodyParser(&body)
+	if err != nil {
+		c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error": "connot parse json",
+		})
+
+	}
+
+	// Throws Unauthorized error
+	if body.Email != "nasuhyc@gmail.com" || body.Password != "123123" {
+		c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"error": "Bad Credentials",
+		})
+
+	}
+
+	// Create the Claims
+	claims := jwt.MapClaims{
+		"name": "Nasuh Yücel",
+		"exp":  time.Now().Add(time.Hour * 72).Unix(),
+	}
+
+	// Create token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// Generate encoded token and send it as response.
+	t, err := token.SignedString([]byte(jwtSecret))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	return c.JSON(fiber.Map{"token": t})
+}
+
+func accessible(c *fiber.Ctx) error {
+	return c.SendString("Accessible")
+}
+
+func restricted(c *fiber.Ctx) error {
+	email := c.Locals("email").(*jwt.Token)
+	claims := email.Claims.(jwt.MapClaims)
+	name := claims["name"].(string)
+	return c.SendString("Welcome " + name)
+}
